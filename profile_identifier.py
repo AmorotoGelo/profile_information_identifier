c8b8eef9cda2407cd6ca3f43572d98bee9bc0844
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-11-22 15:41:52 +0800

    Initial pseudocode

---
+++ profile_identifier.py
@@ -0,0 +1,9 @@
+# Open txt file to read informations
+
+# Ask user for a full name
+
+# Read the informations in txt file
+
+# Use for loop to identify the name in txt file
+
+# Ask user if want to input a name again, exit if not
