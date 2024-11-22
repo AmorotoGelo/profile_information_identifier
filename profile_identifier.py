96795fed3b31117e4eae83e25d5b205ee937e84d
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-11-22 16:46:15 +0800

    Profile name can be identify

--- profile_identifier.py
+++ profile_identifier.py
@@ -2,9 +2,11 @@
 with open(".\\user_information.txt", "r") as profile_identifier:
     # Ask user for a full name
     while True:
-        name = input('Input a full name to identify information: ')
+        name = input('Input a full name to identify information: ').strip()
 # Read the informations in txt file
-
+        user_information = profile_identifier.readlines()
 # Use for loop to identify the name in txt file
-
+        for line in user_information:
+            if name in line:
+                print(f"Information for {name} found: {line.strip()} ")
 # Ask user if want to input a name again, exit if not
