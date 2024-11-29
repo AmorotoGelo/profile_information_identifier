13692895b24e831b6152c3c45d3c6e211340a11a
Carl Angelo Amoroto <carlamoroto200526@gmail.com>
2024-11-30 01:50:23 +0800

    Removing some blank lines, adding print line for more organized output

--- profile_identifier.py
+++ profile_identifier.py
@@ -1,30 +1,29 @@
-# Open txt file to read informations
-with open(".\\user_information.txt", "r") as profile_identifier:
-    # Ask user for a full name
-    while True:
-        name = input('Input a full name to identify information: ')
-        
-        
-        # Read the informations in txt file
-        user_information = profile_identifier.read()
-        lines = user_information.split('\n\n')
-        
-        
 
-        # Use for loop to identify the name in txt file
-        for line in lines:
-         if name in line:
-            print('Identified', line.strip())
-            break
-   
-        if name not in line:
-           print('No information can be found')
+    # Ask user for a full name
+while True:
+        name = input('Input a full name to identify information: ').strip()
         
+        # Open txt file to read informations
+        with open('.\\user_information.txt', 'r') as profile_identifier:
+      
+        # Read the informations in txt file
+         user_information = profile_identifier.read()
+         lines = user_information.strip().split('\n\n')
+             
+        # Use for loop to identify the name in txt file
+         for line in lines:
+          if name in line:
+            print('Identified') 
+            print(line.strip())
+            break
+
+         if name not in line:
+          print('No information can be found')
+
         another_entry = input('Do you want to find another user?: ')
         if another_entry != 'yes':
-           break
-               
-            
+          break
+         
         
             
             
